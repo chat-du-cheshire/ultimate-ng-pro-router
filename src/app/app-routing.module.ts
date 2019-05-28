ab8f12a0412910b363e18b8ee5072b6d06cfe5c0
Ivanov-mo <Ivanov-MO@gaz-is.ru>
2019-05-28 18:35:55 +0300

    Custom preloading strategies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
-import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules, PreloadingStrategy, Route} from '@angular/router';
+import {Observable, of} from 'rxjs';
+
+class CustomPreload implements PreloadingStrategy {
+  preload(route: Route, fn: () => Observable<any>): Observable<any> {
+    return (route.data || {}).preload ? fn() : of(null);
+  }
+}
 
 const routes: Routes = [
-  { path: 'dashboard', loadChildren: './modules/dashboard/dashboard.module#DashboardModule' },
+  { path: 'dashboard', loadChildren: './modules/dashboard/dashboard.module#DashboardModule', data: {preload: true} },
   { path: '**', redirectTo: 'mail/folder/inbox' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {enableTracing: false, preloadingStrategy: PreloadAllModules})],
+  providers: [CustomPreload],
+  imports: [RouterModule.forRoot(routes, {enableTracing: false, preloadingStrategy: CustomPreload})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
